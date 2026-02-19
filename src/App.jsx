bbb1fac88d990d58759d5245f57c243a1adb747b
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2026-02-19 23:34:50 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
+import Footer from "./components/footer/Footer";
 // import Footer from "./components/footer/Footer";
 
 function App() {
-	return (
-		<>
-			<Header/>
-			<Promo/>
-			{/* <Footer/> */}
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Promo />
+      <Footer />
+      {/* <Footer/> */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__brand">StarBucks</div>
+
+      <div className="footer__columns">
+        <div className="footer__column">
+          <h4>Main</h4>
+          <ul>
+            <li>
+              <a href="#">Buy</a>
+            </li>
+            <li>
+              <a href="#">More</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__column">
+          <h4>We make</h4>
+          <ul>
+            <li>
+              <a href="#">Process</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__column">
+          <h4>Products</h4>
+          <ul>
+            <li>
+              <a href="#">Cappuccino</a>
+            </li>
+            <li>
+              <a href="#">Fast</a>
+            </li>
+            <li>
+              <a href="#">Fast</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__column">
+          <h4>Events</h4>
+          <ul>
+            <li>
+              <a href="#">Drinks</a>
+            </li>
+            <li>
+              <a href="#">Eat</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer__column">
+          <h4>Contacts</h4>
+          <ul>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="tel:+79999999999">+7-999-999-99-99</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer__actions">
+        <a href="#" className="footer__instagram">
+          📷
+        </a>
+        <button className="footer__scrollTop">↑</button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
