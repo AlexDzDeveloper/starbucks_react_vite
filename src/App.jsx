994bd06216461807f4e7e4cec1c52f47b315d86e
AlexDz <alexdzdevelop@gmail.com>
2026-03-16 14:45:25 +0200

    Work with new Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
 import Footer from "./components/footer/Footer";
+import Delicious from "./components/delicious/Delicious";
 // import Footer from "./components/footer/Footer";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Promo />
+	  <Delicious/>
       <Footer />
       {/* <Footer/> */}
     </>
---
+++ src/components/delicious/Delicious.jsx
@@ -0,0 +1,12 @@
+import './delicious.scss';
+import Caffee from './../../images/delecious/caffee.png';
+import Cooking from './../../images/delecious/cooking.png';
+
+
+function Delicious() {
+	return (<>
+		{/* <h3>hello cooking</h3> */}
+	</>);
+}
+
+export default Delicious;
