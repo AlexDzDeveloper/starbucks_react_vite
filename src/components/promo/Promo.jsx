38e005332c4b08a327e112c89f016388f7b44cfe
AlexDz <alexdzdevelop@gmail.com>
2026-03-18 23:52:51 +0200

    work with delicious component

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -40,9 +40,6 @@ function Promo() {
 							<img src={wave} alt="wave" id='line1'/>
 							<img src={wave} alt="wave" id='line2'/>
 							<img src={wave} alt="wave" id='line3'/>
-							{/* <span><img src={wave} alt="wave" id='line1'/></span> */}
-							{/* <span><img src={wave} alt="wave" id='line2'/></span> */}
-							{/* <span><img src={wave} alt="wave" id='line3'/></span> */}
 						</div>
 					</div>
 				</div>
