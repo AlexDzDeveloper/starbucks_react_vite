09092a2a4403eeb13bd4ac970dc72b28a24afb86
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2026-03-20 22:54:47 +0200

    new comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
 import Footer from "./components/footer/Footer";
 import Delicious from "./components/delicious/Delicious";
+import OurContacts from "./components/ourContacts/ourContacts";
 // import Footer from "./components/footer/Footer";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <>
       <Header />
       <Promo />
-	  <Delicious/>
+      <Delicious />
+      <OurContacts />
       <Footer />
       {/* <Footer/> */}
     </>
