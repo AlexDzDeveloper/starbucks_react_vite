a7f9cde393c9640fc709b18077ca0b9e80c82af6
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2026-04-07 21:54:59 +0300

    33

--- src/components/ourContacts/ourContacts.jsx
+++ src/components/ourContacts/ourContacts.jsx
@@ -17,54 +17,18 @@ function OurContacts() {
 
         <div className="contacts__items">
           <div className="contacts__item">
-            <div className="contacts__icon">
-              <svg viewBox="0 0 24 24">
-                <rect
-                  x="2"
-                  y="2"
-                  width="20"
-                  height="20"
-                  rx="5"
-                  ry="5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeWidth="2"
-                />
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="3"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
+            <div className="contacts__icon"></div>
             <span>@supercoffee</span>
           </div>
 
           <div className="contacts__item">
-            <div className="contacts__icon">
-              <svg viewBox="0 0 24 24">
-                <path
-                  d="M3 5a2 2 0 012-2h2l2 5-2 1a11 11 0 005 5l1-2 5 2v2a2 2 0 01-2 2h-1C8 21 3 16 3 9V8z"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
+            <div className="contacts__icon"></div>
             <span>+7-999-999-99-99</span>
           </div>
         </div>
       </div>
 
-      <div className="contacts__decor">
-        <svg viewBox="0 0 200 200">
-          <path d="M0,150 C80,100 120,100 200,50" />
-          <path d="M0,170 C80,120 120,120 200,70" />
-        </svg>
-      </div>
+      <div className="contacts__decor"></div>
     </section>
   );
 }
