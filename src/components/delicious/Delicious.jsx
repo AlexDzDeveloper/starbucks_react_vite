d0427de26ea5ecf0bcb488066756b9f8ec58f4b6
AlexDz <alexdzdevelop@gmail.com>
2026-04-09 15:13:52 +0300

    fix img

--- src/components/delicious/Delicious.jsx
+++ src/components/delicious/Delicious.jsx
@@ -4,6 +4,7 @@ import Cooking from './../../images/delecious/cooking.png';
 // import video from './../../videos/cooking.mp4';
 import video2 from './../../videos/cooking1.mp4';
 import wave from './../../images/wave1.png';
+import play from './../../images/delecious/play.svg';
 
 
 function Delicious() {
@@ -36,7 +37,13 @@ function Delicious() {
 						<div className="descr">Only in 2021 we have made more than 100,000 orders for you, your loved ones, all of you, and in 2022 we are ready to destroy the market</div>
 						<div className="cooking__video">
 							<video src={video2}></video>
-							<button className='btn btn-active'>Cooking Process</button>
+							<button className='btn btn-active'>
+								{/* <img src={play} alt="play video" /> */}
+								<svg width="20" height="23" viewBox="0 0 20 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+  								<path d="M17.3594 10.6055C17.6839 10.7995 17.6839 11.2688 17.3594 11.4629L2.25683 20.4941C1.92359 20.6934 1.50008 20.4537 1.5 20.0654L1.5 2.00293C1.50008 1.61468 1.92359 1.37495 2.25684 1.57422L17.3594 10.6055Z" stroke="white" strokeWidth="3" strokeMiterlimit="1.55572" />
+								</svg>
+  								Cooking Process
+  							</button>
 						</div>
 					</div>
 				</div>
